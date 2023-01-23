0d446ad6127620711cc149f9adfa424807eca6bf
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2023-01-23 22:26:01 +0600

    carousel btn working

---
+++ src/js/data.js
@@ -0,0 +1,38 @@
+export const data = [
+  {
+    id: 1,
+    img: "./src/images/team/ (1).jpg",
+    name: "Asikur Rahaman",
+    designation: "UI/UX Designer",
+  },
+  {
+    id: 2,
+    img: "./src/images/team/ (2).jpg",
+    name: "Asikur",
+    designation: "Frontend Developer",
+  },
+  {
+    id: 3,
+    img: "./src/images/team/ (3).jpg",
+    name: "Tanvir Alam",
+    designation: "Full Stack Developer",
+  },
+  {
+    id: 4,
+    img: "./src/images/team/ (4).jpg",
+    name: "Sozol Khan",
+    designation: "Backend Developer",
+  },
+  {
+    id: 5,
+    img: "./src/images/team/ (5).jpg",
+    name: "Abu Hasan",
+    designation: "Web Developer",
+  },
+  {
+    id: 6,
+    img: "./src/images/team/ (6).jpg",
+    name: "Shine",
+    designation: "Graphics Desginer",
+  },
+];
--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,77 @@
-import '../css/style.css'
-import javascriptLogo from '../images/javascript.svg'
-// import { setupCounter } from './counter.js'
-import {setupCounter} from '../js/counter'
+import "../css/style.css";
+import { data } from "./data";
+const carouselContainer = document.getElementById("carousel-container");
+const indicatorsContainer = document.getElementById("indicators-container");
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
+let count = 0;
+  prevBtn.style.display = "none";
+  const getBtnHideShow = (count) =>{
+    count === 0 ? prevBtn.style.display = "none" : prevBtn.style.display = "block";
+    count === data.length - 1 ? nextBtn.style.display = "none" : nextBtn.style.display = "block";
+  }
+// previous button event listener function 
+const getPrevBtnEvenListener = () => {
+    if (count > 0) {
+      count--;
+      getBtnHideShow(count)
+      getBtnHideShow(count)
+        let li  =document.createElement('li')
+        li.setAttribute('class', 'text-slate-100 min-w-full flex items-center flex-col')
+        li.innerHTML = `
+        <div class="avatar w-24 ">
+          <img class="w-full h-full rounded-full object-contain bg-slate-200 " src="${data[count].img}" alt="team-member">
+        </div>
+        <h2 class="text-2xl capitalize my-1 font-semibold -tracking-wider">${data[count].name}</h2>
+        <h6 class="text-xl tracking-tight font-extralight mb-1">${data[count].designation}</h6>
+        <div class="contact-info ">
+          <a href="#" class="mr-2 duration-300 ease-in-out hover:text-[#1299F6]">
+            <i class="text-2xl fa-brands fa-square-facebook"></i>
+          </a>
+          <a href="#" class="mr-2 duration-300 ease-in-out hover:text-[#1DA1F2]">
+            <i class="text-2xl fa-brands fa-square-twitter"></i>
+          </a>
+          <a href="#" class="mr-2 duration-300 ease-in-out hover:text-[#0A66C2]">
+            <i class="text-2xl fa-brands fa-linkedin"></i>
+          </a>
+        </div>
+          `;
+         
+        carouselContainer.textContent = "";
+        carouselContainer.appendChild(li)
+}
+}
+// next button event listener function 
+const getNextBtnEventListener = () => {
+    if (count < data.length - 1) {
+        count++;
+        getBtnHideShow(count)
+        let li  =document.createElement('li')
+        li.setAttribute('class', 'text-slate-100 min-w-full flex items-center flex-col')
+        li.innerHTML = `
+        <div class="avatar w-24 ">
+          <img class="w-full h-full rounded-full object-contain bg-slate-200 " src="${data[count].img}" alt="team-member">
+        </div>
+        <h2 class="text-2xl capitalize my-1 font-semibold -tracking-wider">${data[count].name}</h2>
+        <h6 class="text-xl tracking-tight font-extralight mb-1">${data[count].designation}</h6>
+        <div class="contact-info ">
+          <a href="#" class="mr-2 duration-300 ease-in-out hover:text-[#1299F6]">
+            <i class="text-2xl fa-brands fa-square-facebook"></i>
+          </a>
+          <a href="#" class="mr-2 duration-300 ease-in-out hover:text-[#1DA1F2]">
+            <i class="text-2xl fa-brands fa-square-twitter"></i>
+          </a>
+          <a href="#" class="mr-2 duration-300 ease-in-out hover:text-[#0A66C2]">
+            <i class="text-2xl fa-brands fa-linkedin"></i>
+          </a>
+        </div>
+          `;
+         
+        carouselContainer.textContent = "";
+        carouselContainer.appendChild(li)
+  }
+}
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="/vite.svg" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
-
-// setupCounter(document.querySelector('#counter'))
+  //   button event listener  
+  nextBtn.addEventListener("click", getNextBtnEventListener );
+  prevBtn.addEventListener("click", getPrevBtnEvenListener);
