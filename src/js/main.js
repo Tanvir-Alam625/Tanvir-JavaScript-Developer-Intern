b6b696a13fc4c20307443fc266764d31426bde37
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2023-01-25 02:24:09 +0600

    main carousel all done

--- src/js/main.js
+++ src/js/main.js
@@ -4,74 +4,131 @@ const carouselContainer = document.getElementById("carousel-container");
 const indicatorsContainer = document.getElementById("indicators-container");
 const prevBtn = document.getElementById("prev-btn");
 const nextBtn = document.getElementById("next-btn");
-let count = 0;
-  prevBtn.style.display = "none";
-  const getBtnHideShow = (count) =>{
-    count === 0 ? prevBtn.style.display = "none" : prevBtn.style.display = "block";
-    count === data.length - 1 ? nextBtn.style.display = "none" : nextBtn.style.display = "block";
-  }
-// previous button event listener function 
-const getPrevBtnEvenListener = () => {
-    if (count > 0) {
-      count--;
-      getBtnHideShow(count)
-      getBtnHideShow(count)
-        let li  =document.createElement('li')
-        li.setAttribute('class', 'text-slate-100 min-w-full flex items-center flex-col')
-        li.innerHTML = `
-        <div class="avatar w-24 ">
-          <img class="w-full h-full rounded-full object-contain bg-slate-200 " src="${data[count].img}" alt="team-member">
-        </div>
-        <h2 class="text-2xl capitalize my-1 font-semibold -tracking-wider">${data[count].name}</h2>
-        <h6 class="text-xl tracking-tight font-extralight mb-1">${data[count].designation}</h6>
-        <div class="contact-info ">
-          <a href="#" class="mr-2 duration-300 ease-in-out hover:text-[#1299F6]">
-            <i class="text-2xl fa-brands fa-square-facebook"></i>
-          </a>
-          <a href="#" class="mr-2 duration-300 ease-in-out hover:text-[#1DA1F2]">
-            <i class="text-2xl fa-brands fa-square-twitter"></i>
-          </a>
-          <a href="#" class="mr-2 duration-300 ease-in-out hover:text-[#0A66C2]">
-            <i class="text-2xl fa-brands fa-linkedin"></i>
-          </a>
-        </div>
-          `;
-         
-        carouselContainer.textContent = "";
-        carouselContainer.appendChild(li)
-}
-}
-// next button event listener function 
-const getNextBtnEventListener = () => {
-    if (count < data.length - 1) {
-        count++;
-        getBtnHideShow(count)
-        let li  =document.createElement('li')
-        li.setAttribute('class', 'text-slate-100 min-w-full flex items-center flex-col')
-        li.innerHTML = `
-        <div class="avatar w-24 ">
-          <img class="w-full h-full rounded-full object-contain bg-slate-200 " src="${data[count].img}" alt="team-member">
-        </div>
-        <h2 class="text-2xl capitalize my-1 font-semibold -tracking-wider">${data[count].name}</h2>
-        <h6 class="text-xl tracking-tight font-extralight mb-1">${data[count].designation}</h6>
-        <div class="contact-info ">
-          <a href="#" class="mr-2 duration-300 ease-in-out hover:text-[#1299F6]">
-            <i class="text-2xl fa-brands fa-square-facebook"></i>
-          </a>
-          <a href="#" class="mr-2 duration-300 ease-in-out hover:text-[#1DA1F2]">
-            <i class="text-2xl fa-brands fa-square-twitter"></i>
-          </a>
-          <a href="#" class="mr-2 duration-300 ease-in-out hover:text-[#0A66C2]">
-            <i class="text-2xl fa-brands fa-linkedin"></i>
-          </a>
-        </div>
-          `;
-         
-        carouselContainer.textContent = "";
-        carouselContainer.appendChild(li)
-  }
-}
+const prevBtnModal = document.getElementById("prev-btn-modal");
+const nextBtnModal = document.getElementById("next-btn-modal");
+const modalCarouselContainer =document.getElementById('modal-carousel-container');
+const modalClose = document.getElementById('modal-close');
+const carouselChildrens = carouselContainer.children;
+const carouselItemsConvert = Array.from(carouselChildrens);
+const modalCarouselItemsConvert = Array.from(modalCarouselContainer.children);
+let count = 1;
+let modalCount = 1;
 
-  //   button event listener  
-  nextBtn.addEventListener("click", getNextBtnEventListener );
-  prevBtn.addEventListener("click", getPrevBtnEvenListener);
+// for active indicators function 
+const activeIndicator = () => {
+  let div = document.createElement("div");
+  div.setAttribute("class", "flex");
+  div.innerHTML = ` <i class="mr-2 ${
+    count == 1 ? "fa-solid" : "fa-regular"
+  } fa-circle"></i>
+  <i class=" mr-2 ${count == 2 ? "fa-solid" : "fa-regular"} fa-circle"></i>
+  <i class="mr-2 ${count == 3 ? "fa-solid" : "fa-regular"} fa-circle"></i>
+  <i class="mr-2 ${count == 4 ? "fa-solid" : "fa-regular"} fa-circle"></i>
+  <i class=" mr-2 ${count == 5 ? "fa-solid" : "fa-regular"} fa-circle"></i>
+  <i class="${count == 6 ? "fa-solid" : "fa-regular"} fa-circle"></i>`;
+  indicatorsContainer.textContent = "";
+  indicatorsContainer.appendChild(div);
+};
+// carousel previouse event function 
+const getPrevBtnEvenListener = () => {
+  count < 1 ? (count = carouselChildrens.length - 1) : (count = count);
+
+  const previousCarousel = carouselItemsConvert[count];
+  const filterCarousel = carouselItemsConvert.filter(
+    (caro) => caro !== previousCarousel
+  );
+  previousCarousel.style.display = "flex";
+  previousCarousel.style.transition = "400s ease-in-out";
+  filterCarousel.forEach((element) => {
+    element.style.display = "none";
+    element.style.transition = "400s ease-in-out";
+  });
+  count--;
+  activeIndicator();
+};
+// next button event listener function
+const getNextBtnEventListener = () => {
+  count > 5 ? (count = 0) : (count = count);
+  const previousCarousel = carouselItemsConvert[count];
+  const filterCarousel = carouselItemsConvert.filter(
+    (caro) => caro !== previousCarousel
+  );
+  previousCarousel.style.display = "flex";
+  previousCarousel.style.transition = "transform 400ms ease-in-out";
+  filterCarousel.forEach((element) => {
+    element.style.display = "none";
+    element.style.transition = "transform 400ms ease-in-out";
+  });
+  count++;
+  activeIndicator();
+};
+const getPrevBtnEvenListenerModal = () => {
+  modalCount < 1 ? (modalCount = carouselChildrens.length - 1) : (modalCount = modalCount);
+
+  const previousCarousel = modalCarouselItemsConvert[modalCount];
+  const filterCarousel = modalCarouselItemsConvert.filter(
+    (caro) => caro !== previousCarousel
+  );
+  previousCarousel.style.display = "flex";
+  previousCarousel.style.transition = "400s ease-in-out";
+  filterCarousel.forEach((element) => {
+    element.style.display = "none";
+    element.style.transition = "400s ease-in-out";
+  });
+};
+// next button event listener function
+const getNextBtnEventListenerModal = () => {
+  modalCount > 5 ? (modalCount = 0) : (modalCount = modalCount);
+  const previousCarousel = modalCarouselItemsConvert[modalCount];
+  const filterCarousel = modalCarouselItemsConvert.filter(
+    (caro) => caro !== previousCarousel
+    );
+  previousCarousel.style.display = "flex";
+  filterCarousel.forEach((element) => {
+    element.style.display = "none";
+  });
+  count++;
+};
+
+//   button event listener
+nextBtn.addEventListener("click", getNextBtnEventListener);
+prevBtn.addEventListener("click", getPrevBtnEvenListener);
+nextBtnModal.addEventListener("click", getNextBtnEventListenerModal);
+prevBtnModal.addEventListener("click", getPrevBtnEvenListenerModal);
+// auto play carousel 
+let autoPlay = setInterval(getNextBtnEventListener, 1500);
+// clear interval
+let carouselItems = [...carouselItemsConvert, prevBtn, nextBtn];
+
+carouselItems.forEach((item) => {
+  // auto play carousel stop
+  item.addEventListener("mouseover", () => {
+    clearInterval(autoPlay);
+  });
+  // auto play carousel continue
+  item.addEventListener("mouseout", () => {
+    autoPlay = setInterval(getNextBtnEventListener, 1500);
+  });
+});
+//  modal open 
+carouselItemsConvert.map((item,index) =>{
+  item.addEventListener('click', ()=>{
+      let parentDiv =  modalCarouselContainer.parentNode.parentNode
+      parentDiv.style.display = 'flex';
+      modalCount = index
+      let convertModalCarousel = Array.from(modalCarouselContainer.children);
+      let activeCarousel = convertModalCarousel[index];
+      activeCarousel.style.display = 'flex';
+      let restCarousel = convertModalCarousel.filter(c => c !== activeCarousel);
+      restCarousel.map(restC =>{
+        restC.style.display = 'none';
+      })
+
+  })
+})
+// modal close 
+modalClose.addEventListener('click', ()=>{
+  let parentDiv =  modalCarouselContainer.parentNode.parentNode
+  parentDiv.style.display = 'none';
+})
+
