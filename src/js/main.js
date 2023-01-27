41bb64d82163e638aa9fa12a22ea90f51b070594
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2023-01-27 17:05:46 +0600

    all done

--- src/js/main.js
+++ src/js/main.js
@@ -5,13 +5,21 @@ import "../css/style.css";
 const parentDiv = document.querySelector('.parent-div');
 const carouselContainer = document.querySelector('.carousel-container');
 const indicatorsContainer = document.getElementById('indicators-container')
+const modalContainer = document.getElementById('modal-container')
+const modalClose = document.getElementById('modal-close')
 const prevButton = document.getElementById('prev-btn')
 const nextButton = document.getElementById('next-btn')
+const modalPrevButton = document.getElementById('modal-prev-btn')
+const modalNextButton = document.getElementById('modal-next-btn')
 const carouselItems = document.querySelectorAll('.carousel-item');
+const modalCarouselItems = document.querySelectorAll('.modal-carousel-item');
+const modalCarouselContainer = document.querySelector('.modal-carousel-container');
 const indicators = document.querySelectorAll('.indicator');
 // necessary variables 
 let index = 0;
+let modalIndex = 0;
 let carouselWidth  = carouselItems[index].clientWidth;
+let modalCarouselWidth  = modalCarouselItems[index].clientWidth;
 let carouselLength = carouselItems.length - 1; 
 let convertCarouselItems = Array.from(carouselItems);
 let convertIndicators = Array.from(indicators);
@@ -56,7 +64,6 @@ const getNextCarousel = () => {
                 getActiveIndicator(activeIndicator);
         }else{
                 carouselContainer.style.transform = `translate(-${index * carouselWidth + 1}px)`;
-                // convertIndicators[index].style.background = 'red';
                 let activeIndicator = convertIndicators[index];
                 getActiveIndicator(activeIndicator);
         }
@@ -97,6 +104,47 @@ parentDiv.addEventListener('mouseout', ()=>{
         autoPlay = setInterval(getNextCarousel, 2000);
 })
 
+
+// modal event function 
+convertCarouselItems.map((carousel,index)=> {
+        carousel.addEventListener('click', ()=>{
+                modalIndex =index;
+                // is modal initial Index  value  zero 
+                getShowHideBtn();
+                modalContainer.style.transform  = 'scale(1)';
+                modalCarouselContainer.style.transform = `translate(-${index * modalCarouselWidth}px)`;
+        })
+})
+
+
+// close modal 
+modalClose.addEventListener('click', ()=>{
+        modalContainer.style.transform  = 'scale(0)';
+})
+
+// modal button show hide function 
+const getShowHideBtn = () => {
+        console.log(modalIndex);
+        modalIndex === 0 ? modalPrevButton.style.display = 'none': modalPrevButton.style.display = 'block';
+        modalIndex === modalCarouselItems.length -1 ? modalNextButton.style.display = 'none': modalNextButton.style.display = 'block';
+}
+
+// modal prev button 
+modalPrevButton.addEventListener('click', (e)=>{
+        if (modalIndex >= 0 ) {
+                modalIndex--;
+                modalCarouselContainer.style.transform = `translate(-${modalIndex * modalCarouselWidth}px)`;
+                getShowHideBtn();
+        }
+})
+// modal next button 
+modalNextButton.addEventListener('click', (e)=>{
+        if (modalIndex < modalCarouselItems.length -1 ) {
+                modalIndex++;
+                modalCarouselContainer.style.transform = `translate(-${modalIndex * modalCarouselWidth}px)`;
+                getShowHideBtn();
+        }
+})
 
 //  all elements  
 
